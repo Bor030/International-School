598b78d8b90541c53f36f96bfa0feff9339ce2c4
Bor030 <alexsakic@yahoo.com>
2024-05-31 10:29:13 +0200

    init commit

---
+++ server.js
@@ -0,0 +1,12 @@
+
+const express = require('express')
+const app = express()
+
+
+
+app.get('/', (req, res) => {
+
+    res.send('ok')
+})
+
+app.listen(process.env.PORT, () => {console.log('server radi')})
